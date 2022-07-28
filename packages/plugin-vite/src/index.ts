85fa8570abc3d29adf2bae58d40c3b1ecedf288c
ArcherGu <649601376@qq.com>
2022-07-28 08:54:05 +0800

    feat(plugin-vite): support configureForMode
    This hook can override the plugin config by Vite mode ('development' or 'production')

--- packages/plugin-vite/src/index.ts
+++ packages/plugin-vite/src/index.ts
@@ -1,18 +1,34 @@
 import type { AddressInfo } from 'net'
-import type { Plugin } from 'vite'
+import type { Plugin, ResolvedConfig } from 'vite'
 import type { InlineConfig } from '@doubleshot/builder'
 import { build, dev } from '@doubleshot/builder'
 
-export interface VitePluginDoubleshotConfig extends InlineConfig { }
+export interface VitePluginDoubleshotConfig extends InlineConfig {
+  /**
+   * This hook can override the plugin config by Vite mode ('development' or 'production')
+   */
+  configureForMode: (userConfig: InlineConfig, mode: string) => (void | InlineConfig) | Promise<(void | InlineConfig)>
+}
 
-export function VitePluginDoubleshot(userConfig: VitePluginDoubleshotConfig = {}): Plugin[] {
+export function VitePluginDoubleshot(userConfig: Partial<VitePluginDoubleshotConfig> = {}): Plugin[] {
   const PLUGIN_NAME = 'vite-plugin-doubleshot'
 
   let pack: (() => Promise<void>) | undefined
+
+  const configureForMode = async (resolvedConfig: ResolvedConfig) => {
+    if (typeof userConfig.configureForMode === 'function') {
+      const { mode } = resolvedConfig
+      await userConfig.configureForMode(userConfig, mode)
+    }
+  }
+
   return [
     {
       name: `${PLUGIN_NAME}:serve`,
       apply: 'serve',
+      configResolved: async (resolvedConfig) => {
+        await configureForMode(resolvedConfig)
+      },
       configureServer(server) {
         server?.httpServer?.on('listening', async () => {
           if (server.httpServer) {
@@ -27,7 +43,8 @@ export function VitePluginDoubleshot(userConfig: VitePluginDoubleshotConfig = {}
     {
       name: `${PLUGIN_NAME}:build`,
       apply: 'build',
-      async configResolved() {
+      async configResolved(resolvedConfig) {
+        await configureForMode(resolvedConfig)
         pack = await build(userConfig, false)
       },
       async closeBundle() {
