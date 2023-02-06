f4dd3a495e13c1f6e829ecdcb5546f0e29ccbbc5
Edwin Hui <edwinhuish@users.noreply.github.com>
2023-02-06 10:38:44 +0800

    refactor(nest-electron): change return Promise type in IpcTransport (#294)
    * return Promise type in IpcTransport
    
    * Add filter
    
    * fix missing handler in IPC_ON type
    
    * fix test case
    
    BREAKCHANGE: Return from ipc handle changed

--- packages/nest-electron/src/electron.transport.ts
+++ packages/nest-electron/src/electron.transport.ts
@@ -1,13 +1,8 @@
 import { Logger } from '@nestjs/common'
 import type { CustomTransportStrategy, MessageHandler } from '@nestjs/microservices'
 import { Server } from '@nestjs/microservices'
-import { IPC_HANDLE } from './electron.constants'
+import { IPC_HANDLE, IPC_ON } from './electron.constants'
 import { ipcMessageDispatcher } from './transport'
-
-export interface IpcResponse<T> {
-  data: T
-  error?: any
-}
 
 export class ElectronIpcTransport extends Server implements CustomTransportStrategy {
   protected readonly logger: Logger
@@ -17,7 +12,7 @@ export class ElectronIpcTransport extends Server implements CustomTransportStrat
     this.logger = new Logger(name)
   }
 
-  async onMessage(messageChannel: string, type: string, ...args: any[]): Promise<IpcResponse<any> | void> {
+  async onMessage(messageChannel: string, type: string, ...args: any[]): Promise<any | void> {
     try {
       const handler: MessageHandler | undefined = this.messageHandlers.get(messageChannel)
       if (!handler) {
@@ -37,20 +32,12 @@ export class ElectronIpcTransport extends Server implements CustomTransportStrat
 
       const result = await handler(newArgs)
 
-      if (type === IPC_HANDLE) {
-        return {
-          data: result,
-        }
-      }
+      if (type !== IPC_ON)
+        return result
     }
     catch (error) {
       this.logger.error(error)
-      if (type === IPC_HANDLE) {
-        return {
-          data: undefined,
-          error,
-        }
-      }
+      throw error
     }
   }
 
--- packages/nest-electron/test/mock/src/preload.ts
+++ packages/nest-electron/test/mock/src/preload.ts
@@ -1,10 +1,9 @@
 import { contextBridge, ipcRenderer } from 'electron'
-import type { IpcResponse } from '../../../dist'
 
 contextBridge.exposeInMainWorld(
   'electron',
   {
-    chat: (msg: string): Promise<IpcResponse<string>> => ipcRenderer.invoke('chat', msg),
+    chat: (msg: string): Promise<string> => ipcRenderer.invoke('chat', msg),
     printLog: (log: string): void => ipcRenderer.send('print-log', log),
     sendMultiParams: (param1: string, param2: string): void => ipcRenderer.send('multi-params', param1, param2),
     exit: (): void => ipcRenderer.send('exit'),
