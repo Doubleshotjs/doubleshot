87e9ba4188e8fd6239813f550c26e99ca7edde95
archergu <649601376@qq.com>
2023-02-23 15:11:30 +0800

    test(plugin-vite): set ipv4 host for vite dev server

--- packages/plugin-vite/test/mock/vite.config.ts
+++ packages/plugin-vite/test/mock/vite.config.ts
@@ -3,6 +3,9 @@ import { VitePluginDoubleshot } from '../../dist'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+  },
   plugins: [
     VitePluginDoubleshot({
       type: 'electron',
