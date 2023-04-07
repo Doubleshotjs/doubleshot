7ff0cf221e7703202758f6cb173384c7af6340bd
liuyang0826 <1161906592@qq.com>
2023-04-07 12:07:03 +0800

    feat(nest-electron): add isGlobal option (#399)

--- packages/nest-electron/src/interfaces/electron-module-options.interface.ts
+++ packages/nest-electron/src/interfaces/electron-module-options.interface.ts
@@ -3,6 +3,7 @@ import type { BrowserWindow } from 'electron'
 export interface ElectronModuleOptions {
   name?: string
   win: BrowserWindow
+  isGlobal?: boolean
 }
 
 export type ElectronModuleProviderValue = (BrowserWindow) | ({ win: BrowserWindow })
@@ -11,4 +12,5 @@ export interface ElectronModuleAsyncOptions {
   name?: string
   useFactory: (...args: any[]) => Promise<ElectronModuleProviderValue> | ElectronModuleProviderValue
   inject?: any[]
+  isGlobal?: boolean
 }
