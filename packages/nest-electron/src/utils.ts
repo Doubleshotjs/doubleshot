48357a932de99cb3de3d8e87280f20a6e8a2921d
Edwin Hui <edwinhuish@users.noreply.github.com>
2023-09-06 08:07:18 +0800

    style: better code (#566)
    * fix: ignore eslint error
    
    * style: better code

--- packages/nest-electron/src/utils.ts
+++ packages/nest-electron/src/utils.ts
@@ -1,18 +1,14 @@
+// eslint-disable-next-line n/prefer-global/process
 export const isElectron = process.versions.hasOwnProperty('electron')
 
 export function linkPathAndChannel(channel: string, path = '') {
-  path = path.charAt(0) === '/' ? path.slice(1) : path
-  path = path.charAt(path.length - 1) === '/' ? path.slice(0, -1) : path
-  channel = channel.charAt(0) === '/' ? channel.slice(1) : channel
-  channel = channel.charAt(channel.length - 1) === '/' ? channel.slice(0, -1) : channel
-
-  channel = path.length > 0 ? `${path}/${channel}` : channel
+  const merged = [...path.split('/'), ...channel.split('/')].filter(s => s.length).join('/')
 
   return [
-    `/${channel}`,
-    `${channel}/`,
-    `/${channel}/`,
-    channel,
+    `/${merged}`,
+    `${merged}/`,
+    `/${merged}/`,
+    merged,
   ]
 }
 
