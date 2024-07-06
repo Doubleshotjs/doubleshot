a4fe2ff0cdf1d3a08f9a0953c718948b1a474552
archergu <649601376@qq.com>
2024-07-06 21:59:39 +0800

    fix(nest-electron): test tsup build

--- packages/nest-electron/test/hybrid/tsup.config.ts
+++ packages/nest-electron/test/hybrid/tsup.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   name: 'nest-electron-test',
   target: 'node14',
   clean: true,
-  splitting: true,
   entry: ['src/main.ts', 'src/preload.ts'],
   external: ['electron', '@nestjs', '../../../dist'],
 })
