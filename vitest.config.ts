aa7dc1b47b891f5a805d0e77f50e26dadde21bfd
archergu <649601376@qq.com>
2025-11-25 00:17:08 +0800

    chore: update deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
-import type { UserConfig } from 'vitest/node'
+import type { TestUserConfig } from 'vitest/node'
 
-const config: { test: UserConfig } = {
+const config: { test: TestUserConfig } = {
   // 禁止并行测试
   test: {
     testTimeout: 50000,
