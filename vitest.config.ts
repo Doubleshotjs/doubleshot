343bdeb731e1952521aaa843ee06a71cb4b7c2f1
ArcherGu <649601376@qq.com>
2026-02-04 23:47:33 +0800

    test: use default pool

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { configDefaults, defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     testTimeout: 50 * 1000,
-    pool: 'threads',
     exclude: [
       ...configDefaults.exclude,
       '.pnpm-store/**',
